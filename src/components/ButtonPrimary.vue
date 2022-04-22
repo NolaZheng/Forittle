06e58d33e2219893e6d2af4a1dc50ee8557889e8
nolazheng <nola.zheng@zoomo.space>
2022-04-22 18:42:16 +0800

    feat: finish home page

---
+++ src/components/ButtonPrimary.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="btn-container" :style="containerStyle" @click="onClick">
+    <span class="label">開始</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import type { CSSProperties } from 'vue'
+
+export default defineComponent({
+  name: 'ButtonPrimary',
+  props: {
+    label: String,
+    containerStyle: Object as PropType<CSSProperties>,
+  },
+  methods: {
+    onClick() {
+      console.log(this.containerStyle)
+    },
+  },
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.btn-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 366px;
+  height: 54px;
+  background-color: #272f3f;
+}
+.label {
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/HomeScreen.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="container">
+      <img class="image" :src="backgroundImage" />
+      <ButtonPrimary :containerStyle="buttonStyle" />
+      <img class="web-name" src="../assets/web-name.png" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ButtonPrimary from '@/components/ButtonPrimary.vue'
+
+export default defineComponent({
+  name: 'HomeScreen',
+  components: {
+    ButtonPrimary,
+  },
+  props: {
+    //
+  },
+  data() {
+    return {
+      backgroundImage: require('../assets/home-bg.jpeg'),
+      buttonStyle: {
+        position: 'absolute',
+        bottom: '80px',
+      },
+    }
+  },
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container {
+  height: auto;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.image {
+  width: 100%;
+}
+.web-name {
+  position: absolute;
+  width: 120px;
+  top: 10px;
+  left: 0;
+}
+</style>
